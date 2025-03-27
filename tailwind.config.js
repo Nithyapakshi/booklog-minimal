a272426d0268e4fd5f0f5940e18287c1b98ec01a
Nithyapakshi <pakshiraj.ananthan@gmail.com>
2025-03-27 17:27:27 +0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,7 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
-        primary: {
-          DEFAULT: 'hsl(var(--primary))',
-          foreground: 'hsl(var(--primary-foreground))',
-        }
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
